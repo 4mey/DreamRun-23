95960b8e9b5178b39888662dd7d1f198e091c349
4mey <you@example.com>
2023-03-17 17:24:17 +0530

    final?

--- hero.js
+++ hero.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
   const menuBtn = document.querySelector("menu-btn");
   const navigation = document.querySelector("navigation");
   
-  menuBtn.addEventListener("click", () => {
+  menuBtn.addEventListener(".click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("active");
   });
